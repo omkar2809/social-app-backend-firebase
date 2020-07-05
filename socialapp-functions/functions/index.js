65065d5658446c9fed718e76c97044cd55f593f6
omkar2809 <55548995+omkar2809@users.noreply.github.com>
2020-07-05 17:44:41 +0530

    user authentication (signup)

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -6,9 +6,24 @@ const app = express();
 
 admin.initializeApp();
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBqkdmZ6GBLxchSibnPaYbvkFzP2swZ_Z4",
+    authDomain: "socialapp-ab3d1.firebaseapp.com",
+    databaseURL: "https://socialapp-ab3d1.firebaseio.com",
+    projectId: "socialapp-ab3d1",
+    storageBucket: "socialapp-ab3d1.appspot.com",
+    messagingSenderId: "691341990119",
+    appId: "1:691341990119:web:821c498761bf3645a12a42",
+    measurementId: "G-YLDWL5DHZS"
+};
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
+
 app.get('/screams', (req, res) => {
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .orderBy('createdAt', 'desc')
         .get()
         .then(data => {
@@ -33,16 +48,77 @@ app.post('/scream',(req, res) => {
         createdAt : new Date().toISOString()
     };
 
-    admin.firestore()
-        .collection('screams')
+    db.collection('screams')
         .add(newScream)
         .then(doc => {
             res.json({message: `document ${doc.id} created successfully`});
         })
         .catch(err => {
-            res.statusCode(500).json({error : 'something went wrong'});
+            res.status(500).json({error : 'something went wrong'});
             console.log(err);
         });
+});
+
+// signout route
+
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+
+    // TODO valifdate data
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+        .then(doc => {
+            if(doc.exists) {
+                return res.status(400).json({handle: `this handle is already exist`});
+            }
+            else {
+                return firebase
+                .auth()
+                .createUserWithEmailAndPassword(newUser.email, newUser.password)
+            }
+        })
+        .then(data => {
+            userId = data.user.uid;
+            return data.user.getIdToken();
+        })
+        .then(idToken => {
+            token = idToken;
+            const userCredentials = {
+                handle: newUser.handle,
+                email: newUser.email,
+                createdAt: new Date().toISOString(),
+                userId
+            };
+            db.doc(`/users/${newUser.handle}`).set(userCredentials)
+                .then(() => {
+                    return res.status(201).json({token});
+                });
+        })
+        .catch(err => {
+            console.error(err);
+            if(err.code === 'auth/email-already-in-use') {
+                return res.status(400).json({email : 'Email is already in use'});
+            }
+            else{
+                return res.status(500).json({error: err.code});
+            }
+        });
+
+    // firebase
+    //     .auth()
+    //     .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    //     .then(data => {
+    //         return res.status(201).json({message: `user ${data.user.uid} signup successfully`});
+    //     })
+    //     .catch(err => {
+    //         console.error(err);
+    //         return res.status(500).json({error : err.code});
+    //     });
 });
 
 exports.api = functions.https.onRequest(app);
