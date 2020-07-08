e038b4c6dd7c2467be9eca09f76faa27008f2284
omkar2809 <55548995+omkar2809@users.noreply.github.com>
2020-07-08 22:23:26 +0530

    image upload

--- socialapp-functions/functions/index.js
+++ socialapp-functions/functions/index.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 
 const {getAllScreams, postOneScream} = require('./handles/screams');
-const {signup, login} = require('./handles/users');
+const {signup, login, uploadImage} = require('./handles/users');
 const FBAuth = require('./util/FBAuth');
 
 app.get('/screams', getAllScreams);
 app.post('/scream',FBAuth, postOneScream);
 app.post('/signup', signup);
 app.post('/login', login);
+app.post('/user/image', FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
